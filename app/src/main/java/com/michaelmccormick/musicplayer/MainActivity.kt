599905b1288f5fa0e4aea4f31f43d36af339836b
Michael McCormick <MichaelM97@live.co.uk>
2021-09-07 17:02:01 +0100

    Launched `MusicPlayerService` when play button is tapped

--- app/src/main/java/com/michaelmccormick/musicplayer/MainActivity.kt
+++ app/src/main/java/com/michaelmccormick/musicplayer/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.michaelmccormick.musicplayer
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.michaelmccormick.musicplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupPlayButton()
+    }
+
+    private fun setupPlayButton() {
+        binding.playButton.setOnClickListener {
+            startMusicPlayerService()
+        }
+    }
+
+    private fun startMusicPlayerService() {
+        Intent(this, MusicPlayerService::class.java).apply {
+            this.action = MusicPlayerService.ACTION_PLAY
+        }.let {
+            startService(it)
+        }
     }
 }
